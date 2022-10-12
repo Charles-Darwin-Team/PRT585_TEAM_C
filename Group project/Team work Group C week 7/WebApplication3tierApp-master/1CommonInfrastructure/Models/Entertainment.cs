dd8601285c576da8c4fcebcf3d8da18633c84eef
sujanrdm <sujan.rdm2@gmail.com>
2022-10-12 20:16:54 +0930

    SM 001 Under CommonInfrastructure, created Entertainment

---
+++ Group project/Team work Group C week 7/WebApplication3tierApp-master/1CommonInfrastructure/Models/Entertainment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1CommonInfrastructure.Models
+{
+    public class EntertainmentModel
+    {
+        public int EntertainmentId { get; set; } // int
+        public string EntertainmentName { get; set; } // nvarchar(400)
+        public string EntertainmentConfiguration { get; set; }
+    }
+
+}
