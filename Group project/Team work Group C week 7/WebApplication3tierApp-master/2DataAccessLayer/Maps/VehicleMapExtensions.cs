ba37052f1665cba641792c9ce505c4b22c967da7
sujanrdm <sujan.rdm2@gmail.com>
2022-10-12 22:00:50 +0930

    Updated init.cs and created VehicleMapExtenstions.cs

---
+++ Group project/Team work Group C week 7/WebApplication3tierApp-master/2DataAccessLayer/Maps/VehicleMapExtensions.cs
@@ -0,0 +1,37 @@
+﻿using _1CommonInfrastructure.Models;
+using _2DataAccessLayer.Context.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DataAccessLayer.Maps
+{
+    public static class VehicleMapExtensions
+    {
+        public static VehicleModel ToVehicleModel(this Vehicle src)
+        {
+            var dst = new VehicleModel();
+
+            dst.VehicleId = src.VehicleId;
+            dst.VehicleName = src.VehicleName;
+
+            return dst;
+        }
+
+        public static Vehicle ToVehicle(this VehicleModel src, Vehicle dst = null)
+        {
+            if (dst == null)
+            {
+                dst = new Vehicle();
+            }
+
+            dst.VehicleId = src.VehicleId;
+            dst.VehicleName = src.VehicleName;
+
+            return dst;
+        }
+    }
+}
