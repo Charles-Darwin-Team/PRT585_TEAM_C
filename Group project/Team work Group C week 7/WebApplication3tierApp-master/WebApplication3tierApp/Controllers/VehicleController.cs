02972661959484a4cb55b6c8a1ae9b26e7fd461c
sujanrdm <sujan.rdm2@gmail.com>
2022-10-12 22:20:02 +0930

    Added vehicleController.cs and VehicleDto.cs in WebApp

---
+++ Group project/Team work Group C week 7/WebApplication3tierApp-master/WebApplication3tierApp/Controllers/VehicleController.cs
@@ -0,0 +1,59 @@
+﻿using _2DataAccessLayer.Services;
+using _3BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3tierApp.Models;
+
+namespace WebApplication3tierApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class VehicleController : ControllerBase   
+    {
+
+        private readonly IVehicleService _VehicleService;
+        private readonly ILogger<VehicleController> _logger;
+
+        public VehicleController(IVehicleService VehicleService, ILogger<VehicleController> logger)
+        {
+            _VehicleService = VehicleService;
+            _logger = logger;
+        }
+
+        [HttpGet("", Name = "GetAllVehicles")]
+        public async Task<List<VehicleDto>> GetAll()
+        {
+            var result = await _VehicleService.GetAll();
+            return result.Select(x => x.ToVehicleDto()).ToList();
+        }
+
+        [HttpGet("{VehicleId}", Name = "GetVehicle")]
+        public async Task<VehicleDto?> Get(int VehicleId)
+        {
+            var result = await _VehicleService.GetById(VehicleId);
+            return result?.ToVehicleDto();
+        }
+
+        [HttpPost, Route("")]
+        public async Task<int> Create([FromBody] VehicleDto requestDto)
+        {
+            var VehicleModel = requestDto.ToVehicleModel();
+            return await _VehicleService.CreateVehicle(VehicleModel);
+        }
+
+        [HttpPut, Route("update")]
+        public async Task<IActionResult> Update([FromBody] VehicleDto requestDto)
+        {
+            await _VehicleService.UpdateVehicle(requestDto.ToVehicleModel());
+            return Ok();
+        }
+
+        [HttpDelete, Route("{VehicleId}")]
+        public async Task<IActionResult> Delete(int VehicleId)
+        {
+            await _VehicleService.DeleteVehicle(VehicleId);
+            return Ok();
+        }
+    }
+}
---
+++ Group project/Team work Group C week 7/WebApplication3tierApp-master/WebApplication3tierApp/Models/VehicleDto.cs
@@ -0,0 +1,30 @@
+﻿using _1CommonInfrastructure.Models;
+
+namespace WebApplication3tierApp.Models
+{
+    public class VehicleDto
+    {
+        public int VehicleId { get; set; }
+        public string VehicleName { get; set; }
+                        
+    }
+
+    public static class VehicleDtoMapExtensions
+    {
+        public static VehicleDto ToVehicleDto(this VehicleModel src)
+        {
+            var dst = new VehicleDto();
+            dst.VehicleId = src.VehicleId;
+            dst.VehicleName = src.VehicleName;            
+            return dst;
+        }
+
+        public static VehicleModel ToVehicleModel(this VehicleDto src)
+        {
+            var dst = new VehicleModel();
+            dst.VehicleId = src.VehicleId;
+            dst.VehicleName = src.VehicleName;            
+            return dst;
+        }
+    }
+}
