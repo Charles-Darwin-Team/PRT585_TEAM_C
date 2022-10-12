bac19f837b6c4b5f2ab95216d85343c257ae9683
Jagruthi77 <guptha.mahesh@gmail.com>
2022-10-12 21:47:35 +0930

    Added Vehicle.cs

---
+++ Group project/Team work Group C week 7/WebApplication3tierApp-master/2DataAccessLayer/Context/Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DataAccessLayer.Context.Models
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; } // int
+        public string VehicleName { get; set; } // nvarchar(400)
+        public int VehicleYear { get; set; }
+    }
+}
