9bb4f2407a03182ed7c76a0962871016c0ad53d7
Jagruthi77 <guptha.mahesh@gmail.com>
2022-10-12 22:07:38 +0930

    Added IVehicleService.cs, VehicleService.cs

---
+++ Group project/Team work Group C week 7/WebApplication3tierApp-master/3BusinessLogicLayer/Interfaces/IVehicleService.cs
@@ -0,0 +1,19 @@
+﻿using _1CommonInfrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BusinessLogicLayer.Interfaces
+{
+    public interface IVehicleService
+    {
+        Task<VehicleModel?> GetById(int VehicleId);
+        Task<List<VehicleModel>> GetAll();
+
+        Task<int> CreateVehicle(VehicleModel Vehicle);
+        Task UpdateVehicle(VehicleModel Vehicle);
+        Task DeleteVehicle(int VehicleId);
+    }
+}
