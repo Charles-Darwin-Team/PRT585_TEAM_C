8b63f5a18ad82d57d9013f50305b551d2f398496
sujanrdm <sujan.rdm2@gmail.com>
2022-10-12 21:41:33 +0930

    Added VehicleModel.cs

---
+++ Group project/Team work Group C week 7/WebApplication3tierApp-master/1CommonInfrastructure/Models/VehicleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1CommonInfrastructure.Models
+{
+    public class VehicleModel
+    {
+        public int VehicleId { get; set; } // int
+        public string VehicleName { get; set; } // nvarchar(400)
+
+    }
+
+}
